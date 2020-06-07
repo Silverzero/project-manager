3f9bff0df0059262bdf001b50cb37b94547a8cb9
silverzero <marcodes@ucm.es>
2020-06-07 17:45:01 +0200

    init

---
+++ tests/Feature/ProjectTest.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Project;
+use App\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Laravel\Passport\Passport;
+use Tests\TestCase;
+
+class ProjectTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function graphQL($query){
+        return $this->post('/graphql', [
+            'query' => $query
+        ]);
+    }
+
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testIndexProject()
+    {
+        #prepare
+        $user = factory(User::class)->create();
+        Passport::actingAs($user);
+        factory(Project::class,1)->create();
+        #execution
+        $response = $this->graphQL("{
+                                            project(id: 1){
+                                                id
+                                                name
+                                                budget
+                                            }
+                                          }");
+
+        #assertions
+        $response->assertJson([
+            'data' => [
+                'project' => [
+                    'id' => 1
+                ]
+            ]
+        ]);
+    }
+
+
+    public function testReadProjects()
+    {
+        #prepare
+        $user = factory(User::class)->create();
+        Passport::actingAs($user);
+        factory(Project::class,5)->create();
+        #execution
+        $response = $this->graphQL("{
+                                            projects {
+                                                id
+                                                name
+                                                budget
+                                            }
+                                          }");
+
+        #assertions
+        $response->assertJson([
+            'data' => [
+                'projects' => []
+            ]
+        ]);
+
+    }
+
+    public function testUpdateProject()
+    {
+        #prepare
+        $user = factory(User::class)->create();
+        Passport::actingAs($user);
+        factory(Project::class,1)->create();
+        #execution
+        $response = $this->graphQL('mutation {
+                                            updateProject(
+                                            id:1
+                                            input:	{
+                                              name: "Project Rename"
+                                            }
+                                          ) {
+                                            id
+                                            name
+                                            budget
+                                          }
+                                        }');
+
+        #assertions
+        $response->assertJson([
+            'data' => [
+                'updateProject' => [
+                    'id' => 1,
+                    'name' => "Project Rename"
+                ]
+            ]
+        ]);
+    }
+
+    public function testDeleteProject()
+    {
+        #prepare
+        $user = factory(User::class)->create();
+        Passport::actingAs($user);
+        factory(Project::class,1)->create();
+        #execution
+        $response = $this->graphQL('mutation {
+                                            deleteProject(id:1 ) {
+                                                id
+                                                name
+                                                budget
+                                            }
+                                         }');
+
+        #assertions
+        $response->assertJson([
+            'data' => [
+                'deleteProject' => [
+                    'id' => 1
+                ]
+            ]
+        ]);
+    }
+}
